a2dd64d55e315da062e88c744b509018fc81b4bc
finn-sharp <penguin-klg@naver.com>
2026-01-15 01:27:23 +0900

    chapter2 - regular expressions search vs findall, greedy vs non-greedy, extraction:() closed #2

--- connect_on/webscrapping/regular_expressions.py
+++ connect_on/webscrapping/regular_expressions.py
@@ -17,7 +17,6 @@
 
 
 import re
-from unittest import case
 # re.search()  # Search for a pattern in a string
 # re.findall() # Find all occurrences of a pattern in a string
 
@@ -73,7 +72,73 @@ class Chapter1:
                 if re.search('^X-\S+:', line): # includes 'From:' and '@'
                     print(line)
 
+class chapter2:
+    def __init__(self, case):
+        self.hand = open('./data/mbox-short.txt')
+        self.case = case
 
+    def findall_list(self):
+        """
+            Description : re.findall() 활용 - 리스트 반환
+            [] : 한 문자
+            [0-9] : 0부터 9까지의 숫자
+            [AEIOU] : 대문자 모음
+            [a-zA-Z0-9] : 대소문자 알파벳과 숫자
+            + : 1개 이상 반복
+        """
+        for line in self.hand: # line단위로 순회
+            line = line.rstrip()
+
+            if self.case == 'numbers':
+                if re.findall('[0-9]+', line): 
+                    print(line)
+                    break
+            
+            if self.case == 'uppercase':
+                if re.findall('[A-Z]+', line): 
+                    print(line)
+                    break
+
+    def greedy_exp(self):
+        """
+            Description : re.findall() 활용 - 탐욕적 표현 vs 비탐욕적 표현
+            .* : 0개 이상의 문자 (탐욕적)
+            .*? : 0개 이상의 문자 (비탐욕적)
+        """
+        for line in self.hand: # line단위로 순회
+            line = line.rstrip()
+
+            if self.case == 'greedy':
+                y = re.findall('^F.*:', line) 
+                if len(y) > 0:
+                    print(y)
+                    break
+            
+            if self.case == 'non-greedy':
+                y = re.findall('^F.*?:', line) 
+                if len(y) > 0:
+                    print(y)
+                    break
+
+    def extraction_skill(self):
+        """
+            Description : re.findall() 활용 - 추출 스킬
+            () : 추출 시작과 끝 지정
+        """
+        for line in self.hand: # line단위로 순회
+            line = line.rstrip()
+
+            if self.case == 'extract': # extract 적용 + @의 좌우로 Greedy 적용
+                y = re.findall('^From: (\S+@\S+)', line) 
+                if len(y) > 0:
+                    print(y)
+                    break
+
+            if self.case == 'non-extract': # extract 없이 전체 매칭 + @의 좌우로 Greedy 적용
+                y = re.findall('^From: \S+@\S+', line) 
+                if len(y) > 0:
+                    print(y)
+                    break
 
 if __name__ == "__main__":
     chapter1_1 = Chapter1(case='find')
@@ -88,3 +153,20 @@ if __name__ == "__main__":
     chapter1_4 = Chapter1(case='\S+')
     chapter1_4.whitespace_exp()
     
+    chapter2_1 = chapter2(case='numbers')
+    chapter2_1.findall_list()
+
+    chapter2_2 = chapter2(case='uppercase')
+    chapter2_2.findall_list()
+
+    chapter2_3 = chapter2(case='greedy')
+    chapter2_3.greedy_exp() 
+
+    chapter2_4 = chapter2(case='non-greedy')
+    chapter2_4.greedy_exp()
+
+    chapter2_5 = chapter2(case='extract')
+    chapter2_5.extraction_skill()
+
+    chapter2_6 = chapter2(case='non-extract')
+    chapter2_6.extraction_skill()

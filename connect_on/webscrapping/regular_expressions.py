249b70a1a0462d67751624cd1e639ec893fffc89
finn-sharp <penguin-klg@naver.com>
2026-01-15 11:13:40 +0900

    chapter3 - regular expressions search return : string.find, string.split, re.findall() with (), escape style : //, (escape char) closed #3

--- connect_on/webscrapping/regular_expressions.py
+++ connect_on/webscrapping/regular_expressions.py
@@ -28,6 +28,7 @@ class Chapter1:
     def inner_func_search(self):
         """
             Description : 내장 함수를 활용해서 이메일 추출
+            return : index
         """
         for line in self.hand: # line단위로 순회
             line = line.rstrip()
@@ -68,11 +69,11 @@ class Chapter1:
                 if re.search('^X-*:', line): # includes 'From:' and '@'
                     print(line)
 
-            if self.case == '\S+': # non-whitespace character 1 or more times
-                if re.search('^X-\S+:', line): # includes 'From:' and '@'
+            if self.case == '\\S+': # non-whitespace character 1 or more times
+                if re.search('^X-\\S+:', line): # includes 'From:' and '@'
                     print(line)
 
-class chapter2:
+class Chapter2:
     def __init__(self, case):
         self.hand = open('./data/mbox-short.txt')
         self.case = case
@@ -129,15 +130,64 @@ class chapter2:
             line = line.rstrip()
 
             if self.case == 'extract': # extract 적용 + @의 좌우로 Greedy 적용
-                y = re.findall('^From: (\S+@\S+)', line) 
+                y = re.findall('^From: (\\S+@\\S+)', line) 
                 if len(y) > 0:
                     print(y)
                     break
 
             if self.case == 'non-extract': # extract 없이 전체 매칭 + @의 좌우로 Greedy 적용
-                y = re.findall('^From: \S+@\S+', line) 
+                y = re.findall('^From: \\S+@\\S+', line) 
                 if len(y) > 0:
                     print(y)
+                    break
+
+class Chapter3:
+    def __init__(self, case):
+        self.hand = open('./data/mbox-short.txt')
+        self.case = case
+        
+    def search_return(self):
+        """
+            Description : find, double split, regular expression extraction 비교
+        """
+        data = ''           
+        
+        for line in self.hand: # line단위로 순회
+            line = line.rstrip()
+            y = re.findall('^From: \\S+@\\S+', line) 
+            if len(y) > 0 :
+                data = line + ' ' + 'end'
+                break
+
+        if self.case == 'index':                    
+            atpos = data.find('@')
+            sppos = data.find(' ', atpos)
+            host = data[atpos+1:sppos]
+            print(host)
+
+        if self.case == 'split':
+            words = data.split(' ')
+            email = words[1]
+            pieces = email.split('@')
+            print(pieces[1])
+        
+        if self.case == 'extract':
+            host = re.findall('^From: .*@([^ ]*)', data) # [^ ] : 공백이 아닌 문자   
+            print(*host)
+
+    def escape_char(self):
+        """
+            Description : re.findall() 활용 - 특수문자 이스케이프
+            '\'를 붙여서 특수문자(.)를 일반문자로 인식, 또는 [.] (.)
+        """
+        
+        for line in self.hand: # line단위로 순회
+            line = line.rstrip()
+
+            if self.case == 'escape':
+                y = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line) # 숫자 혹은 점(.)
+                if len(y) > 0:
+                    print(*y)
                     break
 
 if __name__ == "__main__":
@@ -153,20 +203,32 @@ if __name__ == "__main__":
     chapter1_4 = Chapter1(case='\S+')
     chapter1_4.whitespace_exp()
     
-    chapter2_1 = chapter2(case='numbers')
+    chapter2_1 = Chapter2(case='numbers')
     chapter2_1.findall_list()
 
-    chapter2_2 = chapter2(case='uppercase')
+    chapter2_2 = Chapter2(case='uppercase')
     chapter2_2.findall_list()
 
-    chapter2_3 = chapter2(case='greedy')
+    chapter2_3 = Chapter2(case='greedy')
     chapter2_3.greedy_exp() 
 
-    chapter2_4 = chapter2(case='non-greedy')
+    chapter2_4 = Chapter2(case='non-greedy')
     chapter2_4.greedy_exp()
 
-    chapter2_5 = chapter2(case='extract')
+    chapter2_5 = Chapter2(case='extract')
     chapter2_5.extraction_skill()
 
-    chapter2_6 = chapter2(case='non-extract')
+    chapter2_6 = Chapter2(case='non-extract')
     chapter2_6.extraction_skill()
+
+    chapter3_1 = Chapter3(case='index')
+    chapter3_1.search_return()
+
+    chapter3_2 = Chapter3(case='split')
+    chapter3_2.search_return()
+
+    chapter3_3 = Chapter3(case='extract')
+    chapter3_3.search_return()
+
+    chapter3_3 = Chapter3(case='escape')
+    chapter3_3.escape_char()
